7ebf23a4ea75bdb8072bd8b8a5f11bee58d0da21
Saran Pariyar <spariyar05@gmail.com>
2024-07-26 19:44:54 +0545

    Made formField component

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,10 +1,16 @@
 import { View, Text, ScrollView, Image } from "react-native";
-import React from "react";
+import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../../constants";
+import FormField from "../../components/FormField";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -17,6 +23,21 @@ const SignIn = () => {
           <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
             Log in to Aora
           </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            //we'll destructure form and modify email
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
